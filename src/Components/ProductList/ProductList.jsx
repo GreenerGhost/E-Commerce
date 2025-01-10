60e9121d495b8c3bc8f681f5a4b5b86bfcb08ea1
GreenerGhost <Edg_const@hotmail.com>
2025-01-09 23:02:37 -0600

    Actualización de ProductList con llamada a api, muestreo de productos y estilos de ProductList

--- src/Components/ProductList/ProductList.jsx
+++ src/Components/ProductList/ProductList.jsx
@@ -1,8 +1,36 @@
+import { useState, useEffect } from "react";
 import "./ProductList.css";
 
 export default function ProductList() {
+
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState(null);
+
+
+  // Se ejecuta solo una vez, cuando se monta el componente
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://api-ten-jet.vercel.app/products");
+        // Valida que la respuesta sea valida, de lo contrario marca  un error 
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        // Si los datos son validos entonces se transforman los datos obtenidos en formato json
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        setError(error.message);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <section className="main-content">
+
       <aside className="filters">
         <h2>Filtros</h2>
         <div className="filters-category">
@@ -53,6 +81,21 @@ export default function ProductList() {
               </select>
             </label>
           </div>
+        </div>
+
+        <div className="products">
+          { // Se realiza una evaluación del error, si especifica un error se mostrará el error, en caso contrario se mostrara la información de los productos 
+          error ? (
+            <p className="error-message">Hubo un error en la carga de productos: {error}</p>
+          ) : (
+            products.map( ( product ) => (
+              <div key={ product.id } className="product-card">
+                <img src={ product.image } alt={product.name} className="product-image"/>
+                <h3>{ product.nombre }</h3>
+                <p>${ product.precio }</p>
+              </div>
+            ))
+          )}
         </div>
       </main>
     </section>
