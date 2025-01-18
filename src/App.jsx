f53e80df257c7c29b513174c6c8912314fee828d
GreenerGhost <Edg_const@hotmail.com>
2025-01-17 22:24:30 -0600

    Creación del contexto de carro de compras

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,24 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Home from "./Components/Home/Home"
 import Navbar from "./Components/Navbar/Navbar"
 import DetailsProducts from "./Components/DetailsProducts/DetailsProducts"
+import { CartProvider } from "./Components/CartContext/CartContext"
 
 function App() {
 
   return (
     <>
-      <Router>
-        <Navbar/>
-        <Routes>
-          <Route path="/" exact element={<Home />} />
-          <Route path="/product/:id" exact element={<DetailsProducts />} />
-        </Routes>
-      </Router>
+     {/* Se provee el contexto del carro de compras */} 
+      <CartProvider>
+        {/* Se utiliza Route para especificar las rutas y los componentes que se van a renderizar */} 
+        <Router>
+          <Navbar/>
+          {/* Se utiliza Routes para navegar entre componentes */} 
+          <Routes>
+            <Route path="/" exact element={<Home />} />
+            <Route path="/product/:id" exact element={<DetailsProducts />} />
+          </Routes>
+        </Router>
+      </CartProvider>
     </>
   )
 }
---
+++ src/Components/CartContext/CartContext.jsx
@@ -0,0 +1,38 @@
+import React, { Children, createContext, useContext, useState } from "react";
+
+const CartContext = createContext();
+
+export const CartProvider = ( { children } ) => {
+
+  // Se implementan las constantes para los estados del carro de compras
+  const [shoppingCart, setShoppingCart] =useState([]);
+
+  // Se implementa la función para añadir productos al carro de compras
+  const addToCart = (product) => {
+    setShoppingCart( (previousCart) => {
+
+      // Se verifica si el producto ya está en el carro de compras
+      const existingProduct = previousCart.findIndex(
+        (item) => item.id === product.id
+      );
+
+      // Verifica el resultado de findIndex que devuelve 0 o 1 si el producto está en el carro de compras, de lo contrario regresa un -1, por lo que se añadirá el producto al carro de compras
+      if (existingProduct >= 0) {
+        const updatedCart = [...previousCart];
+        updatedCart[existingProduct].quantity += 1;
+        return updatedCart;
+      } else {
+        return [...previousCart, {...product, quantity: 1 }];
+      }
+    });
+  };
+
+  return (
+    // Provider envuelve a los componentes en un proveedor de contexto para especificar el valor de este contexto para todos los componentes hijos
+    <CartProvider.Provider value = { { shoppingCart, addToCart } }>
+      { children }
+    </CartProvider.Provider>
+  )
+};
+
+export const useCart = () => useContext( ( CartContext ) )
